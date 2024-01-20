5a3a4219c11909fe351c160ac43042ecb0446cae
pratikpoojary <pratik.poojary@somaiya.edu>
2024-01-20 22:55:27 +0530

    footer added

--- src/utility/Layout.jsx
+++ src/utility/Layout.jsx
@@ -1,12 +1,15 @@
 import {Outlet, outlet} from "react-router-dom"
 import Header from "./Header"
-
+import Footer from "./Footer"
 export default function Layout(){
 
     return(
     <>
         <Header/>
-        <Outlet/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer/>
     </>
     )
 }
